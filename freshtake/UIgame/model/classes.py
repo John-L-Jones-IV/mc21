5c26fad1b43bb1a90eb75d8edb50a8c20bbfb8bc
John L. Jones IV <johnny.l.jones.iv@gmail.com>
2021-08-08 15:48:38 -0400

    nevermind, big files are better than code existing in a million pieces in a million places... for now (maybe)

--- freshtake/UIgame/model/classes.py
+++ freshtake/UIgame/model/classes.py
@@ -4,7 +4,14 @@ from enum import IntEnum, auto
 import copy
 import random
 
+# inital game settings, can be changed
+NUM_DECKS_IN_GAME = 6
+STARTING_CASH = 200
+MIN_BET = 5
+MAX_SPLITS = 4
+BLACKJACK_PAYS = 3.0/2.0
 
+# generally, don't touch these.
 CARD_VALS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 CARD_SUITS = ["hearts", "spades", "clubs", "diamonds"]
 
@@ -53,8 +60,9 @@ class Card:
         return str(self.value) + " " + str_suit
 
 class Hand:
-    def __init__(self):
+    def __init__(self, bet=None):
         self.hand = []  # List of Card
+        self.bet = bet
 
     def __iter__(self):
         return self.hand.__iter__()
@@ -65,8 +73,14 @@ class Hand:
     def __len__(self):
         return self.hand.__len__()
 
-    def __getitem__(self, key:int) -> Card:
+    def __getitem__(self, key: int) -> Card:
         return self.hand[key]
+    
+    def set_bet(self, wager):
+        self.wager = wager
+
+    def get_bet(self):
+        return self.wager
 
     def pop(self):
         return self.hand.pop()
@@ -146,31 +160,27 @@ class Deck:
 
 
 class Player:
-    def __init__(self, starting_cash):
+    def __init__(self, bankroll=STARTING_CASH):
         self.hands = [Hand()]   # list of Hand to handle splits
         self.active_hand_index = 0
-        self.bankroll = starting_cash
+        self.bankroll = bankroll
 
-    def split_hand(self):
-        hand_idx = self.active_hand_index
-        new_hand = Hand()
-        new_hand.add_card(self.get_hand().pop())
-        self.hands.insert(hand_idx, new_hand)
-        self.active_hand_index += 1
+    def get_bankroll(self):
+        return self.bankroll
+
+    def add_to_bankroll(self, dollar_amount):
+        self.bankroll += dollar_amount
 
     def get_active_hand_index(self):
         return self.active_hand_index
-
-    def add_card_to_hand(self, card, hand_idx=None):
-        if hand_idx is None:
-            hand_idx = self.active_hand_index
-        card.set_showing(True)
-        self.hands[hand_idx].add_card(card)
 
     def get_hand(self, hand_idx=None):
         if hand_idx is None:
             hand_idx = self.active_hand_index
         return self.hands[hand_idx]
+
+    def get_hands(self):
+        return self.hands
 
     def get_best_hand_value(self, hand_idx=None):
         if hand_idx is None:
@@ -182,12 +192,9 @@ class Player:
             hand_idx = self.active_hand_index
         return self.hands[hand_idx].get_second_best_value()
 
-    def get_hands(self):
-        return self.hands
-
     def set_wager(self, wager):
         if type(wager) is not int or wager < 0:
-            raise ValueError("wager must be an int greater than 0")
+            raise ValueError("Wager must be an int greater than 0")
         self.wager = wager
 
     def is_hand_blackjack(self, hand_idx=None):
@@ -199,15 +206,30 @@ class Player:
         if hand_idx is None:
             hand_idx = self.active_hand_index
         return self.hands[hand_idx].is_bust()
-    
+
+    def split_hand(self):
+        hand_idx = self.active_hand_index
+        new_hand = Hand()
+        new_hand.add_card(self.get_hand().pop())
+        self.hands.insert(hand_idx, new_hand)
+        self.active_hand_index += 1
+
+    def add_card_to_hand(self, card, hand_idx=None):
+        if hand_idx is None:
+            hand_idx = self.active_hand_index
+        card.set_showing(True)
+        self.hands[hand_idx].add_card(card)
+
     def move_all_cards(self, destination: Deck()):
         assert isinstance(destination, Deck)
-        hands = copy.deepcopy(self.hands)
-        for hand_cnt, hand in enumerate(hands):
-            for card in hand:
-                destination.append(self.hands[hand_cnt].pop())
+        copy_hands = copy.deepcopy(self.hands)
+        for hand_cnt, copy_hand in enumerate(copy_hands):
+            for copy_card in copy_hand:
+                copy_card.set_showing(False)
+                destination.append(copy_card)
+                self.hands[hand_cnt].pop()
 
-    
+
 class Dealer:
     def __init__(self):
         self.hand = Hand()
@@ -224,9 +246,11 @@ class Dealer:
         return self.hand[1]
 
     def move_all_cards(self, destination: list()):
-        hand = copy.deepcopy(self.hand)
-        for i, card in enumerate(hand):
-            destination.append(self.hand.pop())
+        copy_hand = copy.deepcopy(self.hand)
+        for i, copy_card in enumerate(copy_hand):
+            copy_card.set_showing(False)
+            destination.append(copy_card)
+            self.hand.pop()
 
 
 class GameState(IntEnum):
@@ -235,3 +259,40 @@ class GameState(IntEnum):
     DEAL_CARDS = auto()
     PLAY = auto()
     EVALUATE_RESULTS = auto()
+
+
+class Game:
+    def __init__(self):
+        self.deck = Deck(NUM_DECKS_IN_GAME)
+        self.discard_pile = Deck(0)
+        self.player1 = Player(STARTING_CASH)
+        self.dealer = Dealer()
+        self.state = GameState.DEAL_CARDS
+        self.players = [player1] # list for expandability
+        self.active_player_index = 0
+        self.players_and_dealer = players + [dealer]
+
+    def get_state(self) -> GameState:
+        return self.state
+    
+    def clear_table(self):
+        for player in self.players_and_dealer:
+            player.move_all_cards(self.discard_pile)
+
+    def evaluate_hands(self):
+        dealer_score = self.dealer.get_hand().get_best_value_as_int()
+
+        for player in self.players:
+            for hand in player.get_hands():
+                if hand.is_blackjack() and not dealer.hand.is_blackjack():
+                    winnings = hand.get_bet() * BLACKJACK_PAYS
+                    player.add_to_bankroll(winnings)
+                elif hand.is_bust():
+                    losses = -1 * hand.get_bet()
+                    player.add_to_bankroll(losses)
+                elif hand.get_value_as_int() > dealer_score:
+                    winnings = hand.get_bet()
+                    player.add_to_bankroll(winnings)
+                elif hand.get_value_as_int() < dealer_score:
+                    losses = -1 * hand.get_bet()
+                    player.add_to_bankroll(losses)
